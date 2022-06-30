37ea663f43aed86c6b40a4f079725e4c95b66b3f
Lianne Dakanno <liannedakanno@gmail.com>
2022-06-30 02:09:19 -0500

    commit tree 0e53b6ee675fa866ab0027f9a33fae4a6f7506d6

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if (location < 42)
+        return 42 - location
+    else (location > 42) 
+        return location - 42
+}
+
+function distanceFromHqInFeet(location) {
+    if (location < 42)
+        return (42 - location) * 264
+    else (location > 42)
+        return (location - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination)
+        return (destination - start) * 264
+    else (start > destination) 
+        return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400)
+        return 0;
+
+            if (distanceTravelledInFeet(start, destination) > 2500)
+                return "cannot travel that far";
+
+                    if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000)
+                        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    
+    else (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
+        return 25;
+}
